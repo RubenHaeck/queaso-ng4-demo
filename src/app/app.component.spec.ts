b99184168931e20ee08989a3114ac4738190e46a
Ruben Haeck <ruben.haeck@wol.be>
2017-08-09 14:40:37 +0200

    add @uirouter/angular + ngd

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,10 +17,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'my queaso spotify demo web app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('my queaso spotify demo web app');
   }));
 
   it('should render title in a h1 tag', async(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { UIRouterModule, StateObject } from '@uirouter/angular';
 
 import { AppComponent } from './app.component';
 import { SpotifyComponent } from './spotify/spotify.component';
 import { SpotifySearchComponent } from './spotify-search/spotify-search.component';
 import { SpotifySearchResultComponent } from './spotify-search-result/spotify-search-result.component';
+import { AboutComponent } from './about/about.component';
+
+const helloState: any = { name: 'hello', url:'/hello', component: SpotifyComponent};
+const aboutState: any = { name: 'about', url:'/about', component: AboutComponent};
 
 @NgModule({
   declarations: [
     AppComponent,
     SpotifyComponent,
     SpotifySearchComponent,
-    SpotifySearchResultComponent
+    SpotifySearchResultComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UIRouterModule.forRoot({ states: [helloState, aboutState], useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
